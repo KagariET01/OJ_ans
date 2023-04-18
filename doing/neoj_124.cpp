847de78a25a18a040ef30ec7b2c2d973cba32bb7
ET01 <kagariet01@outlook.com>
2023-04-18 20:37:31 +0800

    WA

--- doing/neoj_124.cpp
+++ doing/neoj_124.cpp
@@ -1,115 +1,67 @@
-/*
-[Q]
-[]
-*/
 #include<bits/stdc++.h>
 using namespace std;
-#define INT int
-#define PII pair<INT,INT>
-#define endl "\n"
 
-bool operator==(PII a,PII b){
-	return (a.first==b.first && a.second==b.second);
-}
+void solve(int,int,int);
+void goodsolve(int,int,int,int,int,int);
 
-void Report(int x1,int y1,int x2,int y2,int x3,int y3);
+void Report(int,int,int,int,int,int);
 
 
 
 
-
-void pairre(PII a,PII b,PII c){
-	Report(a.first,a.second,b.first,b.second,c.first,c.second);
+void solve(int N,int X,int Y){
+	goodsolve(0,N-1,0,N-1,X-1,Y-1);
 }
 
 void goodsolve(int lx,int rx,int ly,int ry,int airx,int airy){
-	//cerr<<"dbg "<<lx<<" "<<rx<<" "<<ly<<" "<<ry<<" "<<airx<<" "<<airy<<endl;
-	if(rx==lx+1){
-		PII a,b,c,d;
-		a={lx,ry};
-		b={rx,ry};
-		c={lx,ly};
-		d={rx,ly};
-		PII nw={airx,airy};
-		if(a==nw){
-			pairre(b,c,d);
-		}else if(b==nw){
-			pairre(a,c,d);
-		}else if(c==nw){
-			pairre(a,b,d);
-		}else{
-			pairre(a,b,c);
-		}
+	if(rx==lx){
+		return;
 	}else{
-		PII a,b,c,d;
-		INT Ax=(lx+rx)/2;
-		INT Ay=(lx+rx)/2;
+		pair<int,int> a,b,c,d;
+		int Ax=(lx+rx)/2;
+		int Ay=(lx+rx)/2;
 		a={Ax,Ay};
 		b={a.first+1,a.second+0};
 		c={a.first+0,a.second+1};
 		d={a.first+1,a.second+1};
-		if(airx <= Ax && airy <= Ay){//<^
+		if(airx <= Ax && airy <= Ay){
 			goodsolve(lx,Ax,ly,Ay,airx,airy);
 			goodsolve(Ax+1,rx,ly,Ay,Ax+1,Ay);
 			goodsolve(lx,Ax,Ay+1,ry,Ax,Ay+1);
 			goodsolve(Ax+1,rx,Ay+1,ry,Ax+1,Ay+1);
-			pairre(b,c,d);
-		}else if(airx>Ax && airy<=Ay){//>^
+			Report(b.first+1,b.second+1,c.first+1,c.second+1,d.first+1,d.second+1);
+		}else if(airx>Ax && airy<=Ay){
 			goodsolve(lx,Ax,ly,Ay,Ax,Ay);
 			goodsolve(Ax+1,rx,ly,Ay,airx,airy);
 			goodsolve(lx,Ax,Ay+1,ry,Ax,Ay+1);
 			goodsolve(Ax+1,rx,Ay+1,ry,Ax+1,Ay+1);
-			pairre(a,c,d);
-		}else if(airx<=Ax && airy>Ay){//<V
+			Report(a.first+1,a.second+1,c.first+1,c.second+1,d.first+1,d.second+1);
+		}else if(airx<=Ax && airy>Ay){
 			goodsolve(lx,Ax,ly,Ay,Ax,Ay);
 			goodsolve(Ax+1,rx,ly,Ay,Ax+1,Ay);
 			goodsolve(lx,Ax,Ay+1,ry,airx,airy);
 			goodsolve(Ax+1,rx,Ay+1,ry,Ax+1,Ay+1);
-			pairre(a,b,d);
+			Report(a.first+1,a.second+1,b.first+1,b.second+1,d.first+1,d.second+1);
 		}else{
 			goodsolve(lx,Ax,ly,Ay,Ax,Ay);
 			goodsolve(Ax+1,rx,ly,Ay,Ax+1,Ay);
 			goodsolve(lx,Ax,Ay+1,ry,Ax,Ay+1);
 			goodsolve(Ax+1,rx,Ay+1,ry,airx,airy);
-			pairre(a,b,c);
+			Report(a.first+1,a.second+1,b.first+1,b.second+1,c.first+1,c.second+1);
 		}
-
 	}
 }
-void solve(int N,int X,int Y){
-	goodsolve(0,N-1,0,N-1,X-1,Y-1);
+
+
+
+void Report(int a,int b,int c,int d,int e,int f){
+	cout<<"Report("<<a<<","<<b<<","<<c<<","<<d<<","<<e<<","<<f<<");"<<endl;
 }
-
-int main(){return 0;}
-void Report(int x1,int y1,int x2,int y2,int x3,int y3){}
-
-/*
-
-
 
 int main(){
 	int n,x,y;
 	cin>>n>>x>>y;
+	cout<<endl<<"solve("<<n<<","<<x<<","<<y<<");"<<endl;
 	solve(n,x,y);
 	return 0;
 }
-void Report(int x1,int y1,int x2,int y2,int x3,int y3){
-	cout<<x1+1<<" "<<y1+1<<" "<<x2+1<<" "<<y2+1<<" "<<x3+1<<" "<<y3+1<<endl;
-}
-
-
-/*
-
-
-
-/*
-+---+---+
-|   |   |
-| A | B |
-|   |   |
-+---+---+
-|   |   |
-| C | D |
-|   |   |
-+---+---+
-*/
