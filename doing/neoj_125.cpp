355a976d6e04bbf7125ee1244f693fc25a321bbe
ET01 <kagariet01@outlook.com>
2023-04-20 20:51:07 +0800

    a

--- doing/neoj_125.cpp
+++ doing/neoj_125.cpp
@@ -8,6 +8,8 @@ using namespace std;
 #define endl "\n"
 
 vector<INT> vec;
+vector<INT> vecadd;
+
 bool debug=1;
 const INT mod=10000019;
 
@@ -16,57 +18,15 @@ INT solve(INT l,INT r){
 	INT mnt=l+(r-l)/2;
 	INT re=0;
 	re+=solve(l,mnt);
-	re=re%10000019;
-	re+=solve(mnt+1,r);
-	re=re%10000019;
-	INT lst[r-l+1];
-	INT nwit=0,lit=l,rit=mnt+1;
-	INT rtt=0;
-	for(INT i=l;i<=mnt;i++){
-		while(rit<=r && lst[rit]<lst[i]){
-			rtt+=lst[rit];
-			rit++;
-		}
-		re+=rtt;
-		re=re%10000019;
-		re+=lst[i]*(rit-(mnt+1));
-		re=re%10000019;
+	re%=mod;
+	re+=solve(mnt,r);
+	re%=mod;
+
+	INT lit=l,rit=mnt,ind=0,temp[r-l];
+	//nl=左指標,nr=右指標,ind=目前temp的.endl(),temp為排序後的list
+	for(;lit<mnt;lit++){//move lit
+		
 	}
-
-
-
-
-
-	rit=mnt+1;
-	//sort
-	while(lit<=mnt && rit<=r){
-		if(vec[lit]>vec[rit]){
-			lst[nwit]=vec[rit];
-			nwit++;
-			rit++;
-		}else{
-			lst[nwit]=vec[lit];
-			nwit++;
-			lit++;
-		}
-	}
-	while(rit<=r){
-		lst[nwit]=vec[rit];
-		nwit++;
-		rit++;
-	}
-	while(lit<=mnt){
-		re+=vec[lit]+rtt;
-		lst[nwit]=vec[lit];
-		nwit++;
-		lit++;
-	}
-
-	nwit=0;
-	for(INT i=l;i<=r;i++,nwit++){
-		vec[i]=lst[nwit];
-	}
-
 
 	return re;
 }
@@ -75,11 +35,19 @@ int main(){
 	cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
 	INT n;
 	while(cin>>n){
-		vec.resize(n);
-		for(INT i=0;i<n;i++){
+		vec.resize(n+1);
+		vecadd.resize(n+1);
+		vecadd[0]=0;
+		for(INT i=1;i<=n;i++){
 			cin>>vec[i];
+			vecadd[i]=vecadd[i-1]+vec[i];//前綴和
 		}
-		cout<<solve(0,n-1);
+		cout<<solve(1,n+1);
 	}
 	return 0;
 }
+
+/*
+參考資料
+https://peienwu.com/sprout6-1/
+*/
