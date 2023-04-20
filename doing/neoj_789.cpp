a2e3e1e7692259691a879a6b7a73acb5596a898c
ET01 <kagariet01@outlook.com>
2023-04-20 22:56:14 +0800

    add neoj_789

---
+++ doing/neoj_789.cpp
@@ -0,0 +1,13 @@
+/*
+[Q]
+[]
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define INT long long int
+#define endl "\n"
+
+int main(){
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+	return 0;
+}
