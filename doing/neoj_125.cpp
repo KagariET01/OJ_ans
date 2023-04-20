b8fe8269bb1753b12868ea637f2aea3586d7f8d7
ET01 <kagariet01@outlook.com>
2023-04-20 21:06:38 +0800

    a

--- doing/neoj_125.cpp
+++ doing/neoj_125.cpp
@@ -15,7 +15,7 @@ const INT mod=10000019;
 
 INT solve(INT l,INT r){
 	if(l==r)return 0;
-	INT mnt=l+(r-l)/2;
+	INT mnt=l+(r-l)/2;//l區的右界(不包含)，r區的左界(包含)
 	INT re=0;
 	re+=solve(l,mnt);
 	re%=mod;
@@ -25,9 +25,30 @@ INT solve(INT l,INT r){
 	INT lit=l,rit=mnt,ind=0,temp[r-l];
 	//nl=左指標,nr=右指標,ind=目前temp的.endl(),temp為排序後的list
 	for(;lit<mnt;lit++){//move lit
-		
+		while(rit<r && vec[rit]<vec[lit]){
+			INT tme=mnt-lit;//除了自己之外，l區裡面比自己大的(在自己右邊的)都要算
+			re+=vecadd[mnt-1]-vecadd[lit-1];//每次+逆序數對，都要加自己一次
+			re%=mod;
+			re+=vec[rit]*tme;
+			re%=mod;
+			temp[ind]=vec[rit];
+			ind++;
+			rit++;
+		}
+		temp[ind]=vec[lit];
+		ind++;
+	}
+	while(rit<r){
+		temp[ind]=vec[rit];
+		rit++;
+		ind++;
 	}
 
+	ind=0;
+	for(int i=l;i<r;i++){
+		vec[i]=temp[ind];
+		ind++;
+	}
 	return re;
 }
 
@@ -42,7 +63,7 @@ int main(){
 			cin>>vec[i];
 			vecadd[i]=vecadd[i-1]+vec[i];//前綴和
 		}
-		cout<<solve(1,n+1);
+		cout<<solve(1,n+1)%mod;
 	}
 	return 0;
 }
