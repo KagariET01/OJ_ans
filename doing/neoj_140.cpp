d37559c00c37b8f2e56534355b75b981f5f5d6c1
ET01 <kagariet01@outlook.com>
2023-04-26 20:50:02 +0800

    add neoj_140
    on branch 'main'
    up to date with 'origin/main'.
    Changes to be committed:
    	new file:   doing/neoj_140.cpp

---
+++ doing/neoj_140.cpp
@@ -0,0 +1,26 @@
+/*
+[Q]
+[]
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define INT long long int
+#define endl "\n"
+#define read(n) reader<n>()
+#define DBG if(debug)
+bool debug=0;
+const INT mxn=1e5;
+INT mod=1e6+7;
+INT dp[mxn+5];
+template<typename TPE>TPE reader(){
+	TPE re;cin>>re;return re;
+}
+
+int main(){
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+	dp[1]=3;
+	INT t=read(INT);
+	while(t--){
+	}
+	return 0;
+}
