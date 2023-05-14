9efebdbf0fedfa0c94cbd532a7ac7efb7e384856
ET01 <66681962+kagariet01@users.noreply.github.com>
2023-05-14 12:04:12 +0800

    新增計算幾何模板

---
+++ Algorithm/math/Geometry/Geometry.cpp
@@ -0,0 +1,18 @@
+/*
+[Q]
+[]
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define INT long long int
+#define endl "\n"
+#define read(n) reader<n>()
+
+bool debug=0;
+
+struct ETmath{
+	struct vec{
+		INT x=0,y=0;
+		void 
+	};
+};
