47c34506d61352205c5e33db8376771219577575
ET01 <66681962+kagariet01@users.noreply.github.com>
2023-05-14 15:16:49 +0800

    更新幾何圖形 新增多邊形struct 新增面積計算

--- Algorithm/math/Geometry/Geometry.cpp
+++ Algorithm/math/Geometry/Geometry.cpp
@@ -13,6 +13,7 @@ bool debug=0;
 template<typename TPE> struct ETmath{
 	/*
 		向量Vec
+		也可當點使用
 	*/
 	struct Vec{
 		TPE x=0,y=0;
@@ -27,25 +28,34 @@ template<typename TPE> struct ETmath{
 	}
 
 	/*
-		點Pnt
+		線段轉向量
+		提供兩點座標，即可算出兩點向量
+		a連到b
 	*/
-	struct Pnt{
-		TPE x=0,y=0;
-		void setv(const TPE &a,const TPE &b){
-			x=a,y=b;
-		}
-	};
-	Pnt make_Pnt(const TPE &a,const TPE &b){
-		Pnt re;
-		re.x=a,re.y=b;
+	Vec Line_Vec(const Vec &a,const Vec &b){
+		Vec re;
+		re.x=b.x-a.x;
+		re.y=b.y-a.y;
 		return re;
+	}
+	Vec operator-(const Vec &a,const Vec &b){
+		return Line_Vec(a,b);
 	}
 
 	/*
-		點連成向量
+		向量轉線段
+		提供向量及點，即可算出延伸出去的點
+		a為起始點，b為向量
+		a和b交換不會有差
 	*/
-	Vec Pnt_Vec(const Pnt &a,const Pnt &b){
-
+	Vec Vec_Pnt(const Vec &a,const Vec &b){
+		Vec re;
+		re.x=b.x+a.x;
+		re.y=b.y+a.y;
+		return re;
+	}
+	Vec operator+(const Vec &a,const Vec &b){
+		return Vec_Pnt(a,b);
 	}
 
 	/*
@@ -64,8 +74,10 @@ template<typename TPE> struct ETmath{
 	}
 	
 	/*
-		外機^
-		AB外積=A的長度*B的長度*sin(AB夾角)
+		外積^
+		AB外積=A的長度*B的長度*sin(AB夾角)=OAB三角形面積
+		B的長度*sin(AB夾角)=OAB的高(底為OA)
+
 		外積=0:兩向量平行
 		外積>0 B 向量在 A 向量的逆時針方向
 		外積<0 B 向量在 A 向量的順時針方向
@@ -75,5 +87,35 @@ template<typename TPE> struct ETmath{
 	}
 	TPE operator^(const Vec &a,const Vec &b){
 		return cross(a,b);
+	}
+
+	/*
+		多邊形Polygon
+		裡面存的是點
+	*/
+	struct Polygon{
+		vector<Vec> vec;
+		void push_back(Vec a){
+			vec.push_back(a);
+		}
+		long long int size(){
+			return vec.size();
+		}
+	};
+
+	/*
+		面積Area
+		將O(0,0)視為開始點
+		連線向量O->P_1和O->P_2......
+		對向量V1和V2計算三角形面積
+		全部總和即為所求
+	*/
+	TPE Area(const Polygon Pol){
+		TPE re=0;
+		long long int Polsz=Pol.size();
+		for(INT i=0;i<Polsz;i++){
+			re+=Pol.vec[i]^Pol.vec[(i+1)%Polsz];
+		}
+		return re/2;
 	}
 };
