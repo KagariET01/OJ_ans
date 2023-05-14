067c330e3112bcca03acdd4f600e2afacc09d5d1
ET01 <66681962+kagariet01@users.noreply.github.com>
2023-05-14 13:12:42 +0800

    新增外積與內積

--- Algorithm/math/Geometry/Geometry.cpp
+++ Algorithm/math/Geometry/Geometry.cpp
@@ -11,16 +11,42 @@ using namespace std;
 bool debug=0;
 
 struct ETmath{
+	/*
+		向量vec
+	*/
 	struct vec{
 		INT x=0,y=0;
 		void setv(const INT &a,const INT &b){
 			x=a,y=b;
 		}
 	};
-	long long int Dot_Product(vec a,vec b){
+	
+	/*
+		內積*
+		AB內積=A的長度*B的長度*cos(AB夾角)
+		內積=0:兩向量垂直
+		內積>0 夾角<90度
+		內積<0 夾角>90度
+	*/
+	long long int Dot(const vec &a,const vec &b){
 		return a.x*b.x+a.y*b.y;
 	}
-	long long int operator*(vec a,vec b){
-		return Dot_Product(vec a,vec b);
+	long long int operator*(const vec &a,const vec &b){
+		return Dot(a,b);
+	
+	}
+	
+	/*
+		外機^
+		AB外積=A的長度*B的長度*sin(AB夾角)
+		外積=0:兩向量平行
+		外積>0 B 向量在 A 向量的逆時針方向
+		外積<0 B 向量在 A 向量的順時針方向
+	*/
+	long long int cross(const vec &a,const vec &b){
+		return a.x*b.y-a.y*b.x;
+	}
+	long long int operator^(const vec &a,const vec &b){
+		return cross(a,b);
 	}
 };
