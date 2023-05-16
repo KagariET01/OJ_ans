32bc511fcbe7950432fbecf9585e7f024f77a7c5
ET01 <66681962%2Bkagariet01@users.noreply.github.com>
2023-05-16 19:42:24 +0800

    [+]cf_1682B.cpp

---
+++ doing/cf_1682B.cpp
@@ -0,0 +1,20 @@
+/*
+[Q]
+[]
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define INT long long int
+#define endl "\n"
+
+int main(){
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+	INT t;cin>>t;
+	while(t--){
+		INT n;cin>>n;
+		INT lst[n];for(INT i=0;i<n;i++)cin>>lst[i];
+		sort(lst,lst+n);
+		
+	}
+	return 0;
+}
