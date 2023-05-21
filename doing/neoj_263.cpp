f3aad6efe8c15e2b5c39983290bbe7dc5ca8c338
ET01 <66681962+kagariet01@users.noreply.github.com>
2023-05-21 17:25:24 +0800

    新增更多VIM使用方法 add neoj_263.cpp

---
+++ doing/neoj_263.cpp
@@ -0,0 +1,24 @@
+/*
+[q]
+[]
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define endl "\n"
+#define read(n) reader<n>()
+#define dbg if(debug)
+bool debug=0;
+template<typename tpe>tpe reader(){
+	tpe re;cin>>re;return re;
+}
+
+int main(){
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+	int t=read(int);
+	while(t--){
+	}
+	return 0;
+}
+
