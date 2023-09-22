b7e958a85f968cc114aa7facbf1320f9ade625cc
ET01@Z13 <66681962+kagariet01@users.noreply.github.com>
2023-09-22 18:57:50 +0800

    a

---
+++ codeforces/cf_1867B.cpp
@@ -0,0 +1,71 @@
+/*
+[q]
+[]
+*/
+//#ifndef eval
+#include<bits/stdc++.h>
+using namespace std;
+#define INT long long int
+#define endl "\n"
+#define read(n) reader<n>()
+#define DBG if(debug)
+#define PII pair<INT,INT>
+bool debug=0;
+bool noTLE=1;
+template<typename tpe>tpe reader(){
+	tpe re;cin>>re;return re;
+}
+
+int main(int argc,char** argv){
+	for(int i=0;i<argc;i++){
+		string nwstr=argv[i];
+		if(nwstr=="-Dev"){
+			debug=1;
+			noTLE=0;
+		}else if(nwstr=="-TLE"){
+			noTLE=0;
+		}
+	}
+	if(noTLE && !debug)cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+	DBG{
+		cout<<"Temp by KagariET01"<<endl;
+		cout<<"My Webpage: https://kagariet01.github.io/about"<<endl;
+		cout<<"===DBG mod on==="<<endl;
+		cout<<"Here's your CFG"<<endl;
+		for(int i=0;i<argc;i++){
+			string nwstr=argv[i];
+			cout<<'['<<nwstr<<']'<<endl;
+		}
+		cout<<"===Code start==="<<endl;
+	}
+	INT t=read(int);
+	while(t--){
+		INT n;
+		cin>>n;
+		string str;
+		cin>>str;
+		bool s[n];
+		for(INT i=0;i<n;i++)s[i]=str[i]-'0';
+		INT duo=0,sin=0;
+		for(INT i=0;i<n/2;i++){
+			if(s[i]==s[n-i-1])duo++;
+			else sin++;
+		}
+		for(INT i=0;i<sin;i++){
+			cout<<"0";
+		}
+		for(INT i=0;i<duo;i++){
+			cout<<"1";
+			if(n%2)cout<<"1";
+			else cout<<"0";
+		}
+		for(INT i=0;i<duo;i++){
+			if(n%2==0){
+				
+			}
+			cout<<"1";
+		}
+	}
+	return 0;
+}
+//#endif
