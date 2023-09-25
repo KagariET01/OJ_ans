5998d64077046b058bc0dfc0346c98f3465b3281
kagariet01@PCIC_NB3 <kagariet01@outlook.com>
2023-09-25 19:35:46 +0800

    a

---
+++ codeforces/cf_1869D1.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define INT long long int
+#define endl "\n"
+#define read(n) reader<n>()
+#define DBG if(debug)
+#define PII pair<INT,INT>
+bool debug=0;
+bool noTLE=1;
+template<typename tpe>tpe reader(){
+	tpe re;cin>>re;return re;
+}
+
+int main(int argc,char** argv){
+	for(int i=0;i<argc;i++){
+		string nwstr=argv[i];
+		if(nwstr=="-Dev"){
+			debug=1;
+			noTLE=0;
+		}else if(nwstr=="-TLE"){
+			noTLE=0;
+		}
+	}
+	DBG{
+		cout<<"Temp by KagariET01"<<endl;
+		cout<<"My Webpage: https://kagariet01.github.io/about"<<endl;
+		cout<<"===DBG mod on==="<<endl;
+		cout<<"Here's your CFG"<<endl;
+		for(int i=0;i<argc;i++){
+			string nwstr=argv[i];
+			cout<<'['<<nwstr<<']'<<endl;
+		}
+		cout<<"===Code start==="<<endl;
+	}
+
+
+	auto solve=[](INT case){
+		
+	}
+	bool 1case=0;
+	bool ynans=1;
+	string yes="yes";
+	string no="no";
+	if(noTLE && !debug)cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+	INT t=(1case?1:read(int));
+	for(INT i=0;i<t;i++){
+		if(!ynans){
+			solve(i);
+		}else{
+			if(solve(i)){
+				cout<<yes<<endl;
+			}else{
+				cout<<no<<endl;
+			}
+		}
+	}
+	return 0;
+}
